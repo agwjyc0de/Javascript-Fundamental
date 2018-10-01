bff1c4315d6b981a51cd1170765f1f310a52831d
unknown <crystalguy404@gmail.com>
2018-10-01 11:29:23 +0700

    Variable Var,Let dan Const

---
+++ variable.js
@@ -0,0 +1,10 @@
+// Perbedaan var,let dan const
+
+var Nama = "Anggi Wiijaya";
+let NPanggilan = "Wijaya";
+const NamaIstri = "***Tetot***";
+
+//Output Ke Console
+console.log(Nama);
+console.log(NPanggilan);
+console.log(NamaIstri);
